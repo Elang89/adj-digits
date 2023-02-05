217712291c6d079e596f9f45f28d6c63a708f81c
Elang89 <elang.apps@gmail.com>
2023-02-05 10:23:56 -0500

    Add testing

---
+++ README.md
@@ -0,0 +1,13 @@
+# Adjacent Digits
+
+## How to use this program
+
+To use this program you need to install Rust and cargo. Once these tools are installed please run the following command:
+
+`cargo run`
+
+### Running Tests
+
+To run tests please run the following command: 
+
+`cargo test`
--- src/api/api.rs
+++ src/api/api.rs
@@ -1,4 +1,8 @@
 fn check_input(value: &String) -> bool {
+    if value.is_empty() == true {
+        return false;
+    }
+
     for char in value.chars() {
         let result = char.to_string().parse::<u32>();
         if result.is_err() {
@@ -46,4 +50,63 @@ pub fn init(value: String, limit: usize) -> () {
     let (max_str, max_val) = find_number(&value, limit);
 
     println!("Max Multiplication Value: {max_val}; String Value: {max_str}")
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::api::api::multiply;
+
+    use super::{check_input, find_number};
+
+    #[test]
+    fn test_check_input_true() -> () {
+        let val = "13421242".to_string();
+        let result = check_input(&val);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_check_input_false() -> () {
+        let val = "something".to_string();
+        let result = check_input(&val);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_check_input_empty() -> () {
+        let val = "".to_string();
+        let result = check_input(&val);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn test_check_mult() -> () {
+        let val = "123456".to_string();
+        let result = multiply(&val);
+        assert_eq!(result, 720);
+    }
+
+    #[test]
+    fn test_check_mult_complex() -> () {
+        let val = "7385342344".to_string();
+        let result = multiply(&val);
+        assert_eq!(result, 967680);
+    }
+
+    #[test]
+    fn test_check_mult_zero() -> () {
+        let val = "3423420414".to_string();
+        let result = multiply(&val);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_default_string() -> () {
+        let val =
+            "798853072792707819458384342342349704396309603640976205270626701969153".to_string();
+        let result = find_number(&val, 10);
+
+        assert_eq!(result.0, "7819458384");
+        assert_eq!(result.1, 7741440);
+    }
 }
